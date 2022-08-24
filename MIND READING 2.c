e537f4837bfda0ebc0f9e0e32e2130ea23e688a1
PraveenTech005 <92732706+PraveenTech005@users.noreply.github.com>
2022-08-24 19:39:45 +0530

    Add files via upload
    Its Only A 2 Step Calculation Tricks Watch Its Carefully

---
+++ MIND READING 2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("I AM GONNA READ YOUR MIND\n");
+    getc(stdin);
+    printf("DON'T TAKE YOUR CALCULATOR ITS SIMPLE THAN B4\n");
+    getc(stdin);
+    printf("       LETS BEGIN !      \n");
+    getc(stdin);
+    printf("Think Any Number Between 0-9\n");
+    getc(stdin);
+    printf("Multiply It By 5\n");
+    getc(stdin);
+    printf("Multiply It By 2\n");
+    getc(stdin);
+    printf("ADD It With 5\n");
+    getc(stdin);
+    printf("ADD It With 3\n");
+    getc(stdin);
+    printf("Hmmm🤭 Hmmm🤭 ALMOST DONE !\n");
+    getc(stdin);
+    printf("Now Type The Answer Below : ");
+    scanf("%d",&a);
+
+    a = a/10;  
+    printf("HAHA Got It\n"); 
+    printf("The Number That You Pick  : %d\n",a); 
+
+
+
+
+
+}
