0486dfaf7e07723424254aaf9ffa8ec6cafd82fe
PRAVEEN <92732706+PraveenTech005@users.noreply.github.com>
2022-12-23 19:03:55 +0530

    Update MIND READING 1.c

--- MIND READING 1.c
+++ MIND READING 1.c
@@ -36,27 +36,26 @@ int main()
 
     num  = ans / 100;
     alpha = ans % 100;
-   // num1 = num * 100;
-   // chr  = ans - num1;
-   // alpha = chr + 10;
+// num1 = num * 100;
+// chr  = ans - num1;
+// alpha = chr + 10;
     
     printf("HAHA Got It\n"); 
     printf("The Number That You Pick  : %d\n",num);                     //First Digit Is The Number That You Picked
     printf("Alphabet That You Choose  : %c\n",alpha+10);                //Next Two Digit Is The Number For The Character That You Picked
-                                                                        // See Below For Character Reference
-    /*
-    A - 55          N - 68
-    B - 56          O - 69
-    C - 57          P - 70
-    D - 58          Q - 71
-    E - 59          R - 72
-    F - 60          S - 73
-    G - 61          T - 74
-    H - 62          U - 75
-    I - 63          V - 76
-    J - 64          W - 77
-    K - 65          X - 78
-    L - 66          Y - 79
-    M - 67          Z - 80
+/*
+A - 55          N - 68
+B - 56          O - 69
+C - 57          P - 70
+D - 58          Q - 71
+E - 59          R - 72
+F - 60          S - 73
+G - 61          T - 74
+H - 62          U - 75
+I - 63          V - 76
+J - 64          W - 77
+K - 65          X - 78
+L - 66          Y - 79
+M - 67          Z - 80
 */
 }
