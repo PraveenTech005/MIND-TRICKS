e2a59e8d378eeffc0bfc7090ab16e04ff6c4657b
PRAVEEN <92732706+PraveenTech005@users.noreply.github.com>
2022-12-23 19:05:34 +0530

    Update MIND READING 2.c

--- MIND READING 2.c
+++ MIND READING 2.c
@@ -9,24 +9,24 @@ int main()
     getc(stdin);
     printf("       LETS BEGIN !      \n");
     getc(stdin);
-    printf("Think Any Number Between 0-9\n");                       //5
+    printf("Think Any Number Between 0-9\n");         //5
     getc(stdin);
-    printf("Multiply It By 5\n");                                   //5 * 5 = 25
+    printf("Multiply It By 5\n");                     //5 * 5 = 25
     getc(stdin);
-    printf("Multiply It By 2\n");                                   //25 * 2 = 50
+    printf("Multiply It By 2\n");                     //25 * 2 = 50
     getc(stdin);
-    printf("ADD It With 5\n");                                      //50 + 5 = 55
+    printf("ADD It With 5\n");                        //50 + 5 = 55
     getc(stdin);
-    printf("ADD It With 3\n");                                      //55 + 3 = 58
+    printf("ADD It With 3\n");                        //55 + 3 = 58
     getc(stdin);
     printf("Hmmm🤭 Hmmm🤭 ALMOST DONE !\n");
     getc(stdin);
-    printf("Now Type The Answer Below : ");                         //First Digit Of The Answer Is Your Picked Number
+    printf("Now Type The Answer Below : ");           //First Digit Of The Answer Is Your Picked Number
     scanf("%d",&a);
 
-    a = a/10;                                                       //58 / 10 = 5.8
+    a = a/10;                                         //58 / 10 = 5.8
     printf("HAHA Got It\n"); 
-    printf("The Number That You Pick  : %d\n",a);                   //It Prints 5 Due To Integer Declaration
+    printf("The Number That You Pick  : %d\n",a);     //It Prints 5 Due To Integer Declaration
 
 
 
